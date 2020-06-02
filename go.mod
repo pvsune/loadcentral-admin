331a3cbe30c3d8d73ee3833da8a68e23530a9181
Philamer Sune <pvsune@hotmail.com>
2020-06-02 22:25:44 +0700

    Add doSendLoad() method to do actual load sending.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/appleboy/gin-jwt v2.5.0+incompatible
 	github.com/appleboy/gin-jwt/v2 v2.6.3
 	github.com/gin-gonic/gin v1.6.3
+	github.com/google/uuid v1.1.1
 	github.com/spf13/viper v1.7.0
 	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0 // indirect
 )
--- views/admin.go
+++ views/admin.go
@@ -1,7 +1,13 @@
 package views
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/pvsune/loadcentral-admin/config"
+	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -23,4 +29,41 @@ func (admin Admin) SendLoad(c *gin.Context) {
 		return
 	}
 	c.HTML(http.StatusOK, "index.tmpl", nil)
+}
+
+func md5Hex(data []string) string {
+	h := md5.New()
+	for _, d := range data {
+		io.WriteString(h, d)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func doSendLoad(phone_number string, pcode string) {
+	conf := config.GetConfig()
+
+	rrn := uuid.New().String()
+	auth := md5Hex([]string{
+		md5Hex([]string{rrn}),
+		md5Hex([]string{
+			conf.GetString("LC_USERNAME"), conf.GetString("LC_PASSWORD"),
+		}),
+	})
+	requestURL := fmt.Sprintf(
+		"%s/?uid=%s&auth=%s&rrn=%s&pcode=%s&to=%s",
+		conf.GetString("LC_BASEURL"),
+		conf.GetString("LC_USERNAME"),
+		auth, rrn, pcode, phone_number,
+	)
+
+	resp, err := http.Get(requestURL)
+	if err != nil {
+		panic(fmt.Sprintf("LoadCentral Error: %s", err))
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(fmt.Sprintf("LoadCentral Error: %s", err))
+	}
+	fmt.Printf("%s", body)
 }
